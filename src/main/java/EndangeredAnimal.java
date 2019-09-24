12c9b03a5878fc80cc28fcf6140b709e650b9c91
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-24 15:40:55 +0200

    saved location in database

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -6,12 +6,14 @@ public class EndangeredAnimal {
     private String name;
     private String health;
     private String age;
+    private String location;
     private int id;
 
-    public EndangeredAnimal(String name, String health,String age) {
+    public EndangeredAnimal(String name, String health,String age,String location) {
         this.name = name;
         this.health = health;
         this.age = age;
+        this.location=location;
     }
 
     public String getName() {
@@ -23,13 +25,17 @@ public class EndangeredAnimal {
     public String getAge() {
         return age;
     }
+    public String getLocation() {
+        return location;
+    }
     public void save() {
         try (Connection con = DB.sql2o.open()) {
-            String sql = "INSERT INTO endangeredanimal (name,health,age) VALUES (:name,:health,:age)";
+            String sql = "INSERT INTO endangeredanimal (name,health,age,location) VALUES (:name,:health,:age,:location)";
             this.id = (int) con.createQuery(sql, true)
                     .addParameter("name", this.name)
                     .addParameter("health", this.health)
                     .addParameter("age", this.age)
+                    .addParameter("location", this.location)
                     .executeUpdate()
                     .getKey();
         }
